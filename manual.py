4f67ff1fffab651ba53d4d5316656e03653556ab
Duck <saheasy@gmail.com>
2023-05-06 18:43:24 -0500

    Added BCM number mode to fix GPIO RunTime Error

--- manual.py
+++ manual.py
@@ -27,6 +27,7 @@ pins = {
     "right_motor_in1": 16, "right_motor_in2": 18, "right_motor_speed": 22,
     "left_motor_in1": 16, "left_motor_in2": 18, "left_motor_speed": 22,
 }
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(pins["right_motor_in1"], GPIO.OUT, initial=GPIO.HIGH) #Right Motor In1 Setup
 GPIO.setup(pins["right_motor_in2"], GPIO.OUT, initial=GPIO.HIGH) #Right Motor In2 Setup
 GPIO.setup(pins["right_motor_speed"], GPIO.OUT) #Right Motor PWM Setup
