f505cba628f2b880e6b79bac1727373e79496d6f
Duck <saheasy@gmail.com>
2023-05-06 22:33:47 -0500

    changed stepper test file type

--- my-telemetrix.py
+++ my-telemetrix.py
@@ -1,6 +1,5 @@
 """
  Copyright (c) 2021 Alan Yorinks All rights reserved.
-
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
  Version 3 as published by the Free Software Foundation; either
@@ -9,13 +8,9 @@
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  General Public License for more details.
-
  You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
  along with this library; if not, write to the Free Software
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
- DHT support courtesy of Martyn Wheeler
- Based on the DHTNew library - https://github.com/RobTillaart/DHTNew
 """
 
 import sys
@@ -24,18 +19,16 @@ import time
 from telemetrix import telemetrix
 
 """
-Run a motor continuously without acceleration
+Run a motor using runSpeedToPosition position
 """
 
 # Create a Telemetrix instance.
 board = telemetrix.Telemetrix()
 
 
-# for continuous motion, the callback is not used, but provided to meet the
-# API needs.
 def the_callback(data):
     date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data[2]))
-    print(f'Run motor {data[1]} completed motion at: {date}.')
+    print(f'Motor {data[1]} runSpeedToPosition motion completed at: {date}.')
 
 
 # create an accelstepper instance for a TB6600 motor driver
@@ -46,12 +39,15 @@ def the_callback(data):
 motor = board.set_pin_mode_stepper(interface=8, pin1=8, pin2=9, pin3=10, pin4=11)
 
 
-# set the max speed and speed
-board.stepper_set_max_speed(motor, 1000)
-board.stepper_set_speed(motor, 1000)
+# set the max speed and target position
+board.stepper_set_max_speed(motor, 800)
+board.stepper_move_to(motor, 2000)
+
+# set the motor speed
+board.stepper_set_speed(motor, 400)
 
 # run the motor
-board.stepper_run_speed(motor)
+board.stepper_run_speed_to_position(motor, completion_callback=the_callback)
 
 # keep application running
 while True:
