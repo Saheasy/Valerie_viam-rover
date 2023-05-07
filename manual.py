ff3ab46d446e5814c9fd7dc910db814609a9f965
Duck <saheasy@gmail.com>
2023-05-06 22:26:43 -0500

    Took out some PWM code for manual, and added test file.

--- manual.py
+++ manual.py
@@ -35,8 +35,8 @@ try:
     GPIO.setup(pins["right_motor_in1"], GPIO.OUT, initial=GPIO.HIGH) #Right Motor In1 Setup
     GPIO.setup(pins["right_motor_in2"], GPIO.OUT, initial=GPIO.LOW) #Right Motor In2 Setup
     GPIO.setup(pins["right_motor_speed"], GPIO.OUT) #Right Motor PWM Setup
-    right_motor_pwm = GPIO.PWM(22, 0.87)
-    right_motor_pwm.start(50)
+    #right_motor_pwm = GPIO.PWM(22, 0.87)
+    #right_motor_pwm.start(50)
 
     while 1:
         '''
@@ -47,9 +47,12 @@ try:
             keys[event.code] = event.state
         '''
         [ keys.update({event.code: event.state}) for event in get_gamepad() ]
+        
+        '''
         equation = int( (keys["ABS_RZ"] / 127) * 50 )
         right_motor_pwm.ChangeDutyCycle( equation ) 
         print( equation )
+        '''
 
 except KeyboardInterrupt:
     right_motor_pwm.stop()
